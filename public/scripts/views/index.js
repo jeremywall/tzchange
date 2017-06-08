7e059ccd3323684af6f05677a3442738f587e0cd
Jeremy Wall <jeremy.wall@gmail.com>
2017-06-08 11:59:17 -0700

    add bootstrap for UI

--- public/scripts/views/index.js
+++ public/scripts/views/index.js
@@ -17,15 +17,25 @@ $(function() {
   rowsData = _.orderBy(rowsData, ['nextChangeEpochMillis', 'zoneName'], ['asc', 'asc']);
   $.each(rowsData, function(index, rowData) {
     var $tr = $('<tr/>')
-    var diff = Math.floor((rowData.nextChangeEpochMillis - epochMillis) / 1000);
-    $tr.append($('<td/>').text(
-      Math.floor(diff / 86400) + 'd ' +
-      Math.floor(diff % 86400 / 3600) + 'h ' +
-      Math.floor(diff % 86400 % 3600 / 60) + 'm ' +
-      Math.floor(diff % 86400 % 3600 % 60) + 's'
-    ));
+    var diffSecs = Math.floor((rowData.nextChangeEpochMillis - epochMillis) / 1000);
+    var diff = {
+      d: Math.floor(diffSecs / 86400),
+      h: Math.floor(diffSecs % 86400 / 3600) ,
+      m: Math.floor(diffSecs % 86400 % 3600 / 60),
+      s: Math.floor(diffSecs % 86400 % 3600 % 60)
+    };
+    if (diff.d < 30) {
+      $tr.addClass('danger');
+    }
+    $tr.append($('<td/>').text(diff.d + 'd ' + diff.h + 'h ' + diff.m + 'm ' + diff.s + 's'));
     $tr.append($('<td/>').text(rowData.zoneName));
     $tbody.append($tr);
   });
+
+  var $thead = $('<thead/>').append(
+    $('<tr/>').append($('<th/>').text('Countdown')).append($('<th/>').text('Zone'))
+  );
+
+  $('table#countdown thead').replaceWith($thead);
   $('table#countdown tbody').replaceWith($tbody);
 });
