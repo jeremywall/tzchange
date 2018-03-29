59776bf9f7f0b514af2fa7c26c5a5c837812cc27
Jeremy Wall <jeremywall@users.noreply.github.com>
2018-03-29 13:28:00 -0700

    UTC Time of Change

--- public/scripts/views/index.js
+++ public/scripts/views/index.js
@@ -41,6 +41,7 @@ $(function() {
       $tr.addClass('danger');
     }
     $tr.append($('<td/>').text(nextChangeDiff.d + 'd ' + nextChangeDiff.h + 'h ' + nextChangeDiff.m + 'm ' + nextChangeDiff.s + 's '));
+    $tr.append($('<td/>').text(nextChangeEpochMillis));
     $tr.append($('<td/>').text(rowData.zone.name));
     $tr.append($('<td/>').text('UTC' + currOffset.p + _.padStart(currOffset.h, 2, '0') + _.padStart(currOffset.m, 2, '0')));
     $tr.append($('<td/>').text('UTC' + nextOffset.p + _.padStart(nextOffset.h, 2, '0') + _.padStart(currOffset.m, 2, '0')));
@@ -50,6 +51,7 @@ $(function() {
   var $thead = $('<thead/>').append(
     $('<tr/>')
       .append($('<th/>').text('Countdown'))
+      .append($('<th/>').text('UTC Time of Change'))
       .append($('<th/>').text('Zone'))
       .append($('<th/>').text('Offset Before'))
       .append($('<th/>').text('Offset After'))
