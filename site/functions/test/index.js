6e95030c0e087215d28249472bb8e4b349e8bb94
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-09-08 12:54:09 -0700

    add lodash

--- site/functions/test/index.js
+++ site/functions/test/index.js
@@ -1,6 +1,8 @@
+const _ = require("lodash");
+
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World!"})
+        body: JSON.stringify({message: "Hello World! " + _.VERSION})
     };
 }
