7b28acf8d5b8f3b46de224c3bc89555e1d3f49c1
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-09-09 11:18:17 -0700

    content type

--- src/functions/next-change/index.js
+++ src/functions/next-change/index.js
@@ -19,6 +19,9 @@ exports.handler = async function(event, context) {
     data = _.orderBy(data, ['epoch_of_change', 'zone'], ['asc', 'asc']);
     return {
         statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify(data)
     };
 }
