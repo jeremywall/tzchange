4c207d87510c3cbbc032a73be2a55c5a9bf15eee
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-09-09 12:44:38 -0700

    cleanup

--- src/functions/test/index.js
+++
@@ -1,8 +0,0 @@
-const _ = require("lodash");
-
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World!!!! " + new Date().getTime() + " " + _.VERSION})
-    };
-}
