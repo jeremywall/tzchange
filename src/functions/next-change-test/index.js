2f3e87c5772e3e37eff3af042716c2aed7eddf94
Jeremy Wall <jeremywall@users.noreply.github.com>
2022-06-01 21:53:28 -0700

    test

--- src/functions/next-change-test/index.js
+++ src/functions/next-change-test/index.js
@@ -1,22 +1,21 @@
 const _ = require("lodash");
 const moment = require("moment-timezone");
+const tzdata = require('tzdata');
+const tc = require("timezonecomplete");
 
 exports.handler = async function(event, context) {
     let data = [];
-    let zones = moment.tz.names();
-    let nowMoment = moment();
+
+    var tzdb = tc.TzDatabase.instance();
+    var zones = tzdb.zoneNames();
+
     _.each(zones, function(zoneName) {
-      let zone = moment.tz.zone(zoneName);
-      let nextChangeIndex = _.sortedIndex(zone.untils, nowMoment.valueOf());
-      let nextChangeEpochMillis = zone.untils[nextChangeIndex];
-      if (_.isFinite(nextChangeEpochMillis)) {
-        data.push({
-          'epoch_of_change': moment.utc(nextChangeEpochMillis).format('X'),
-          'zone': zoneName
-        });
-      }
+      data.push({
+
+        'zone': zoneName
+      });
     });
-    data = _.orderBy(data, ['epoch_of_change', 'zone'], ['asc', 'asc']);
+
     return {
         statusCode: 200,
         headers: {
