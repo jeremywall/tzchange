fc93c87c32bb703fdf5e73e714f47acf4a128be4
Jeremy Wall <jeremywall@users.noreply.github.com>
2022-06-01 21:57:47 -0700

    test

--- src/functions/next-change-test/index.js
+++ src/functions/next-change-test/index.js
@@ -6,14 +6,27 @@ const tc = require("timezonecomplete");
 exports.handler = async function(event, context) {
     let data = [];
 
-    var tzdb = tc.TzDatabase.instance();
-    var zones = tzdb.zoneNames();
+    let tzdb = tc.TzDatabase.instance();
+    let zones = tzdb.zoneNames();
+
+    let tsMillis = Date.now();
 
     _.each(zones, function(zoneName) {
-      data.push({
-
-        'zone': zoneName
-      });
+      try {
+        var nextChangeEpochMillis = tzdb.nextDstChange(zoneName, tsMillis);
+        if (_.isFinite(nextChangeEpochMillis)) {
+          //console.log('next change for ' + zoneName + ' is ' + nextChangeEpochMillis);
+          data.push({
+            epoch_of_change: nextChangeEpochMillis,
+            zone: zoneName
+          });
+        } else {
+          //console.log("Excluding zone: " + zoneName + " as it has no future DST change");
+        }
+      } catch(error) {
+        console.log("Error with zone: " + zoneName);
+        console.log(error)
+      }
     });
 
     return {
